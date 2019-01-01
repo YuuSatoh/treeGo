7dce367285c9c1b4d59a2d44d6fe3b6367251525
satoyu <satoyu@satoyunoMacBook-puro.local>
2019-01-02 01:23:26 +0900

    update

--- main.go
+++ main.go
@@ -5,40 +5,87 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"cmd"
+	"path/filepath"
+	"strings"
 )
 
+var hasPaentOtherFiles []bool
+
 func main() {
+	dirName, fileName := initInput()
+	runTree(dirName, fileName)
+}
 
-	// 取得
-	p, _ := os.Getwd()
-	fmt.Println(p)
-
-	// var path string
-	path := initOption()
-	fmt.Println(path)
-
-	files, _ := ioutil.ReadDir(path)
-	for _, f := range files {
-		fmt.Println(f.Name())
+func runTree(dir string, file string) {
+	readDirPath := dir + file
+	readedfiles, err := ioutil.ReadDir(readDirPath)
+	if err != nil {
+		fmt.Printf("%s: %s", os.Args[0], err)
+		os.Exit(1)
 	}
 
-    if err := run(); err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        os.Exit(1)
-    }
+	readedFilesLength := len(readedfiles)
+	for index, rededFile := range readedfiles {
 
+		if strings.HasPrefix(rededFile.Name(), ".") {
+			continue
+		}
+
+		isLastFile := isLast(index, readedFilesLength)
+		printTreeLine(rededFile.Name(), isLastFile)
+
+		if rededFile.IsDir() {
+			hasPaentOtherFiles = append(hasPaentOtherFiles, isLastFile)
+			runTree(readDirPath+"/", rededFile.Name())
+		}
+	}
+	hasPaentOtherFiles = deleteSliceLastItem(hasPaentOtherFiles)
 }
 
-func initOption() string {
-	var dirPath string
-	flag.StringVar(&dirPath, "path", "./", "print dir path flag")
+func isLast(index int, len int) bool {
+	return index == len-1
+}
+
+func printTreeLine(fileName string, isLast bool) {
+	for _, hasPaentOtherFile := range hasPaentOtherFiles {
+		if hasPaentOtherFile {
+			fmt.Print("    ")
+		} else {
+			fmt.Print("│   ")
+		}
+	}
+
+	if isLast {
+		fmt.Printf("└─")
+	} else {
+		fmt.Printf("├─")
+	}
+
+	fmt.Println("─", fileName)
+}
+
+func deleteSliceLastItem(slice []bool) []bool {
+	if slice == nil || len(slice) < 1 {
+		return slice
+	}
+	tempSlice := slice[:len(slice)-1]
+	newSlice := make([]bool, len(tempSlice))
+	copy(newSlice, slice)
+	return newSlice
+}
+
+func initInput() (string, string) {
+	var (
+		path string
+		dir  string
+		file string
+	)
+	flag.StringVar(&path, "path", "", "print dir path flag")
 	flag.Parse()
-	return dirPath
-}
-
-
-func run() {
-    cmd := cmd.NewFoobarCommand()
-    return cmd.Execute()
+	dir, file = filepath.Split(path)
+	if dir == "" {
+		dir, _ = os.Getwd()
+	}
+	fmt.Println(dir)
+	return dir, file
 }
